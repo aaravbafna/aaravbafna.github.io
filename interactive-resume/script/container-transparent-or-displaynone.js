694baa6a05aceb855cfa3a78e09f95ce7d68b1b9
ab1137 <31161521+ab1137@users.noreply.github.com>
2020-08-21 21:24:49 +0530

    Website Files

---
+++ interactive-resume/script/container-transparent-or-displaynone.js
@@ -0,0 +1,2 @@
+function setContainerTransparentOrDisplaynone() { "internet explorer" == browserName && browserVersion <= 8 ? containerDiv.setAttribute("class", "displaynone") : containerDiv.setAttribute("class", "transparent") }
+var containerDiv = document.getElementById("container"); setContainerTransparentOrDisplaynone();
